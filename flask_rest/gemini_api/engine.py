ecdf2fa7c30db14534c5ffd90c0d3a56ac496251
eddygulled <edgargulay69@gmail.com>
2024-02-13 15:55:14 +0300

    gemini engine init complete

---
+++ flask_rest/gemini_api/engine.py
@@ -0,0 +1,35 @@
+import textwrap
+import google.generativeai as genai
+from credentials import gemini_key
+
+
+class Engine:
+    def __init__(self, genai):
+        self.genai = genai
+        self.GOOGLE_API_KEY=gemini_key
+        self.genai.configure(api_key=self.GOOGLE_API_KEY)
+        self.model = None
+
+    def list_models(self):
+        _models = []
+        for m in genai.list_models():
+            if 'generateContent' in m.supported_generation_methods:
+                _models.append(m)
+        return _models
+
+    def load_model(self, model_name):
+        self.model = genai.GenerativeModel(model_name)
+
+    def generate_text(self, prompt):
+        if self.model is None:
+            raise ValueError("Model not loaded")
+        return self.model.generate_content(prompt)
+
+
+
+gen_engine = Engine(genai)
+gen_engine.load_model('gemini-pro')
+
+response = gen_engine.generate_text("What is the meaning of life?")
+
+print(response.text)
